0ea7c99750f6871582d8c787a9c53600ec33826d
Denis Mama <denis.mama@vd.ch>
2019-06-06 13:53:04 +0200

    Adaptation DEvEx, à savoir 1 seul fichier en paramètre pour le Main.

--- src/main/java/ch/vd/unireg/config/BatchRptRunnerApp.java
+++ src/main/java/ch/vd/unireg/config/BatchRptRunnerApp.java
@@ -6,74 +6,65 @@ import java.text.DecimalFormatSymbols;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Properties;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.util.StopWatch;
 
-import ch.vd.registre.base.date.RegDate;
+import ch.vd.dfin.tao.batch.common.ind.exception.BatchSystemException;
 import ch.vd.unireg.common.AuthenticationHelper;
 import ch.vd.unireg.listes.afc.ExtractionDonneesRptJob;
-import ch.vd.unireg.listes.afc.TypeExtractionDonneesRpt;
 import ch.vd.unireg.listes.afc.pm.ExtractionDonneesRptPMJob;
-import ch.vd.unireg.listes.afc.pm.ModeExtraction;
-import ch.vd.unireg.listes.afc.pm.VersionWS;
 import ch.vd.unireg.scheduler.BatchScheduler;
 import ch.vd.unireg.scheduler.BatchSchedulerImpl;
 import ch.vd.unireg.scheduler.JobDefinition;
+import ch.vd.unireg.utils.BatchRptProperties;
 
-public class BatchRptRunnerApp {
+/**
+ * Batch d'extraction RPT d'UNIREG
+ */
+public final class BatchRptRunnerApp {
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(BatchRptRunnerApp.class);
 	private static final DecimalFormat DECIMAL_FORMAT_SEC;
+	private static final BatchRptProperties PROPERTIES_INSTANCE = BatchRptProperties.getInstance();
 
 	static {
 		Locale chLocale = (new Locale.Builder()).setRegion("CH").setLanguage("fr").build();
 		DECIMAL_FORMAT_SEC = new DecimalFormat("#.###", DecimalFormatSymbols.getInstance(chLocale));
 		DECIMAL_FORMAT_SEC.setRoundingMode(RoundingMode.HALF_UP);
-	}
-
-	private class BatchRunnertException extends Exception {
-
-		BatchRunnertException(Throwable cause) {
-			super(cause);
-		}
 
 	}
 
+
+
+	/**
+	 * Méthode de démarrage du batch.
+	 *
+	 * @param args les arguments passé en paramètre à l'application, aucun attendu.
+	 */
 	public static void main(String[] args) {
 		final StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 
-		final Properties properties = parseCommandLine(args);
-		if (properties == null) {
-			LOGGER.error("Erreur lors du parsing des parametres");
-			System.exit(1);
+		if (args == null || args.length == 0) {
+			throw new BatchSystemException("Manque l'argument du path et nom du fichier de configuration");
 		}
-		LOGGER.info("Debut du traitement du batch  {} RPT ...", BatchRptRunnerApp.class.getName());
-		LOGGER.info("Parametres  du batch {}: ", "PP".equalsIgnoreCase(properties.getProperty("population")) ? ExtractionDonneesRptJob.NAME : ExtractionDonneesRptPMJob.NAME);
-		LOGGER.info("Periode Fiscale = {}, mode = {}  ", properties.get("periodeFiscale"), properties.get("mode"));
+		System.setProperty("ch.vd.unireg.rpt.batch.arg.propertyFile", args[0]);
+
+		LOGGER.info("---- BATCH D'EXTRACTION RPT UNIREG - DEBUT DU TRAITEMENT ------------------------------------");
+		LOGGER.info("Parametres  du batch {}: ", PROPERTIES_INSTANCE.getJobName());
+		LOGGER.info("Periode Fiscale = {}, mode = {}  ", PROPERTIES_INSTANCE.getPeriodeFiscale(), PROPERTIES_INSTANCE.getModeExtraction());
 
 		try {
-			new BatchRptRunnerApp(properties);
+			demarrerBatch();
 			stopWatch.stop();
-			LOGGER.info("Fin du traitement {}. Duree = {} sec.", BatchRptRunnerApp.class.getName(), formatMsToSecondes(stopWatch.getLastTaskTimeMillis()));
-
+			LOGGER.info("BATCH D'EXTRACTION RPT UNIREG - FIN DU TRAITEMENT {}. Duree = {} sec.", PROPERTIES_INSTANCE.getJobName(), formatMsToSecondes(stopWatch.getLastTaskTimeMillis()));
 			System.exit(0);
 		}
-		catch (BatchRunnertException exp) {
+		catch (BatchSystemException exp) {
 			stopWatch.stop();
 			System.err.println("Erreur d'execution .  Raison: " + exp.getMessage());
 			LOGGER.error("Erreur d'execution .  Raison: {}", exp.getMessage());
@@ -82,33 +73,35 @@ public class BatchRptRunnerApp {
 
 	}
 
-	private BatchRptRunnerApp(Properties properties) throws BatchRunnertException {
 
-		LOGGER.info("Contruction du context spring");
+	private static void demarrerBatch() throws BatchSystemException {
+		// Chargement du context Spring.
+		LOGGER.info("Chargement du context Spring");
 		final ApplicationContext context = new ClassPathXmlApplicationContext("unireg-all-module.xml");
-		final String population = properties.getProperty("population");
-		final BatchScheduler batchScheduler = context.getBean("batchScheduler", BatchSchedulerImpl.class);
+
+		//injection des informations d'authentifications.
 		AuthenticationHelper.pushPrincipal("RPT-Batch");
 
 		try {
-			String jobName;
 			final Map<String, Object> params = new HashMap<>();
-			params.putIfAbsent(ExtractionDonneesRptJob.PERIODE_FISCALE, NumberUtils.toInt(properties.getProperty("periodeFiscale"), RegDate.get().year() - 1));
-			params.putIfAbsent(ExtractionDonneesRptJob.NB_THREADS, 4);
+			params.putIfAbsent(ExtractionDonneesRptJob.PERIODE_FISCALE, PROPERTIES_INSTANCE.getPeriodeFiscale());
+			params.putIfAbsent(ExtractionDonneesRptJob.NB_THREADS, PROPERTIES_INSTANCE.getNombreThreads());
 
-			if ("PP".equalsIgnoreCase(population)) {
-				jobName = ExtractionDonneesRptJob.NAME;
-				params.putIfAbsent(ExtractionDonneesRptJob.MODE, getParamTypeExtraction(properties));
+			if ("PP".equalsIgnoreCase(PROPERTIES_INSTANCE.getPopulation())) {
+				params.putIfAbsent(ExtractionDonneesRptJob.MODE, PROPERTIES_INSTANCE.getTypeExtraction());
 			}
 			else {
-				jobName = ExtractionDonneesRptPMJob.NAME;
-				params.putIfAbsent(ExtractionDonneesRptPMJob.VERSION_WS, getparamVersionWS(properties));
-				params.putIfAbsent(ExtractionDonneesRptPMJob.MODE, getParamModeExtraction(properties));
+				params.putIfAbsent(ExtractionDonneesRptPMJob.VERSION_WS, PROPERTIES_INSTANCE.getparamVersionWS());
+				params.putIfAbsent(ExtractionDonneesRptPMJob.MODE, PROPERTIES_INSTANCE.getModeExtraction());
 			}
 
-			final JobDefinition jobDefinition = batchScheduler.startJob(jobName, params);
+			// Chargement du batchScheduler depuis le context.
+			final BatchScheduler batchScheduler = context.getBean("batchScheduler", BatchSchedulerImpl.class);
+			final JobDefinition jobDefinition = batchScheduler.startJob(PROPERTIES_INSTANCE.getJobName(), params);
+
 			Integer oldpercentProgression = jobDefinition.getPercentProgression();
 
+			// On attend que toutes le job soit terminé
 			while (jobDefinition.isRunning()) {
 				if (oldpercentProgression == null) {
 					oldpercentProgression = jobDefinition.getPercentProgression();
@@ -126,7 +119,7 @@ public class BatchRptRunnerApp {
 
 		}
 		catch (Exception exp) {
-			throw new BatchRunnertException(exp);
+			throw new BatchSystemException(exp.getMessage(), exp);
 		}
 		finally {
 			AuthenticationHelper.popPrincipal();
@@ -134,109 +127,6 @@ public class BatchRptRunnerApp {
 		}
 	}
 
-	@NotNull
-	private ModeExtraction getParamModeExtraction(Properties properties) {
-
-		final String modeStr = properties.getProperty("mode");
-		ModeExtraction mode;
-		try {
-			mode = StringUtils.isNotBlank(modeStr) ? ModeExtraction.valueOf(modeStr) : ModeExtraction.BENEFICE;
-		}
-		catch (IllegalArgumentException ex) {
-			mode = ModeExtraction.BENEFICE;
-		}
-		return mode;
-	}
-
-	@NotNull
-	private TypeExtractionDonneesRpt getParamTypeExtraction(Properties properties) {
-
-		final String modeStr = properties.getProperty("mode");
-		TypeExtractionDonneesRpt mode;
-		try {
-			mode = StringUtils.isNotBlank(modeStr) ? TypeExtractionDonneesRpt.valueOf(modeStr) : TypeExtractionDonneesRpt.REVENU_ORDINAIRE;
-		}
-		catch (IllegalArgumentException ex) {
-			mode = TypeExtractionDonneesRpt.REVENU_ORDINAIRE;
-		}
-		return mode;
-	}
-
-	@NotNull
-	private VersionWS getparamVersionWS(Properties properties) {
-		final String version_ws = properties.getProperty("version_ws");
-		VersionWS versionWS;
-		try {
-			versionWS = StringUtils.isNotBlank(version_ws) ? VersionWS.valueOf(version_ws) : VersionWS.V7;
-		}
-		catch (IllegalArgumentException ex) {
-			versionWS = VersionWS.V7;
-		}
-		return versionWS;
-	}
-
-
-	@NotNull
-	private static Options getOptions() {
-		final Options options = new Options();
-		options.addOption("help", "affiche ce message");
-		options.addOption("population", "[OBLIGATOIRE] Population, valeur possible (PP|PM) exemple -Dpopulation=PP");
-		options.addOption("periodeFiscale", "[FACULTATIF] Période fiscale, année courant -1,   exemple -DperiodeFiscale=2019");
-		options.addOption("mode",
-		                  " [FACULTATIF]Type d'extraction des données de référence RPT à génére. valeur par défaut REVENU_ORDINAIRE pour PP, BENEFICE pour PM . Valeurs possibles pour les PP (REVENU_ORDINAIRE|REVENU_SOURCE_PURE|FORTUNE), les PM (BENEFICE)");
-		options.addOption("version_ws", " [FACULTATIF] version de ws utilisée, valeur par défaut v7.  Valeurs possibles (v6,v7) exemple -Dversion_ws=v7");
-		return options;
-	}
-
-
-	private static Properties parseCommandLine(String[] args) {
-
-		CommandLine commandLine = null;
-		try {
-			final Option propertyOption = Option.builder()
-					.longOpt("D")
-					.argName("property=value")
-					.hasArgs()
-					.valueSeparator()
-					.desc("use value for given properties")
-					.build();
-
-			final Options options = new Options();
-			options.addOption(propertyOption);
-
-
-			// parse the command line arguments
-			final CommandLineParser parser = new DefaultParser();
-			commandLine = parser.parse(options, args);
-			if (commandLine.hasOption("help") || (!commandLine.hasOption("D") && commandLine.getArgs().length != 1)) {
-				final HelpFormatter formatter = new HelpFormatter();
-				formatter.printHelp("BatchRunner [rpt] [options]", "Options:", getOptions(), null);
-				return null;
-			}
-		}
-		catch (ParseException exp) {
-			LOGGER.error("Erreur de parsing.  Raison: {}", exp.getMessage());
-			System.err.println("Erreur de parsing.  Raison: " + exp.getMessage());
-			System.exit(1);
-		}
-
-		final Properties properties = commandLine.getOptionProperties("D");
-
-		validateParameter(properties);
-
-		return properties;
-	}
-
-	private static void validateParameter(Properties properties) {
-
-		final String population = properties.getProperty("population");
-		if (StringUtils.isBlank(population)) {
-			final HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp("BatchRunner [rpt] [options]", "Options:", getOptions(), null);
-		}
-		LOGGER.info("le parsing des paramètres est correcte.");
-		System.out.println("le parsing des paramètres est correcte.");
-	}
 
 	private static String formatMsToSecondes(long duration) {
 		float durationSec = (float) duration / 1000.0F;
---
+++ src/main/java/ch/vd/unireg/utils/BatchRptProperties.java
@@ -0,0 +1,163 @@
+/*
+ * *********************************************
+ * *        CARTOUCHE DES MODIFICATIONS        *
+ * *********************************************
+ *
+ */
+package ch.vd.unireg.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+import ch.vd.dfin.tao.batch.common.ind.exception.BatchSystemException;
+import ch.vd.dfin.tao.batch.common.ind.util.BatchProperties;
+import ch.vd.dfin.tao.batch.common.ind.util.PropertiesFile;
+import ch.vd.registre.base.date.RegDate;
+import ch.vd.unireg.listes.afc.ExtractionDonneesRptJob;
+import ch.vd.unireg.listes.afc.TypeExtractionDonneesRpt;
+import ch.vd.unireg.listes.afc.pm.ExtractionDonneesRptPMJob;
+import ch.vd.unireg.listes.afc.pm.ModeExtraction;
+import ch.vd.unireg.listes.afc.pm.VersionWS;
+
+
+/**
+ * Classe permettant d'acceder aux propriétés du batch de cloture automatique
+ */
+public final class BatchRptProperties extends BatchProperties {
+
+	private static final String PROP_NAME_PERIODE_FISCALE = "rpt.periode.fiscale";
+
+	private static final String PROP_NAME_POPULATION = "rpt.population";
+
+	private static final String PROP_NAME_MODE = "rpt.mode";
+
+	private static final String PROP_NAME_WS = "rpt.version.ws";
+
+	/**
+	 * L'unique instance de ce singleton.
+	 */
+	private static final BatchRptProperties UNIQUE_INSTANCE = new BatchRptProperties();
+
+	/**
+	 * @return l'unique instance de ce singleton.
+	 */
+	public static BatchRptProperties getInstance() {
+		return UNIQUE_INSTANCE;
+	}
+
+
+	/**
+	 * Constructeur privé pour empêcher l'instanciation.
+	 */
+	private BatchRptProperties() {
+		// noop.
+	}
+
+	/**
+	 * @return la période fiscale spécifiée dans le fichier de propriétés.
+	 */
+	public Long getPeriodeFiscale() {
+		Long periodeFiscale;
+
+		String propertyValue = getProperty(PROP_NAME_PERIODE_FISCALE);
+
+		if (StringUtils.isBlank(propertyValue)) {
+			propertyValue = String.valueOf(RegDate.get().year() - 1);
+		}
+		try {
+			periodeFiscale = Long.valueOf(propertyValue);
+		}
+		catch (NumberFormatException e) {
+			throw new BatchSystemException("Le format de la période fiscale est incorrect. [propriété : " + PROP_NAME_PERIODE_FISCALE + "].", e);
+		}
+
+		return periodeFiscale;
+	}
+
+	public String getPopulation() {
+		String propertyValue = getProperty(PROP_NAME_POPULATION);
+
+		if (StringUtils.isBlank(propertyValue)) {
+			throw new BatchSystemException("La propriété [" + PROP_NAME_POPULATION + "] doit obligatoirement être définie.");
+		}
+		return propertyValue;
+	}
+
+	public String getJobName() {
+		return "PP".equals(getPopulation()) ? ExtractionDonneesRptJob.NAME : ExtractionDonneesRptPMJob.NAME;
+	}
+
+	@NotNull
+	public ModeExtraction getModeExtraction() {
+		ModeExtraction mode;
+		final String propertyValue = getProperty(PROP_NAME_MODE);
+		try {
+			mode = StringUtils.isNotBlank(propertyValue) ? ModeExtraction.valueOf(propertyValue) : ModeExtraction.BENEFICE;
+		}
+		catch (IllegalArgumentException ex) {
+			throw new BatchSystemException("La propriété [" + PROP_NAME_MODE + "] doit  être définie avec la bonne valeur.");
+		}
+		return mode;
+	}
+
+	@NotNull
+	public TypeExtractionDonneesRpt getTypeExtraction() {
+		TypeExtractionDonneesRpt mode;
+		final String propertyValue = getProperty(PROP_NAME_MODE);
+		try {
+			mode = StringUtils.isNotBlank(propertyValue) ? TypeExtractionDonneesRpt.valueOf(propertyValue) : TypeExtractionDonneesRpt.REVENU_ORDINAIRE;
+		}
+		catch (IllegalArgumentException ex) {
+			throw new BatchSystemException("La propriété [" + PROP_NAME_MODE + "] doit  être définie avec la bonne valeur.");
+		}
+		return mode;
+	}
+
+	@NotNull
+	public VersionWS getparamVersionWS() {
+		VersionWS versionWS;
+		final String version_ws = getProperty(PROP_NAME_WS);
+		try {
+			versionWS = StringUtils.isNotBlank(version_ws) ? VersionWS.valueOf(StringUtils.upperCase(version_ws)) : VersionWS.V7;
+		}
+		catch (IllegalArgumentException ex) {
+			throw new BatchSystemException("La propriété [" + PROP_NAME_WS + "] doit  être définie avec la bonne valeur.");
+		}
+		return versionWS;
+	}
+
+
+	/**
+	 * Traitement particulier pour récupérer le fichier de config
+	 *
+	 * @return
+	 */
+	@Override
+	protected PropertiesFile getLocalProperties() {
+		synchronized (this) {
+			if (this.properties == null) {
+				String nomFichier = System.getProperty("ch.vd.unireg.rpt.batch.arg.propertyFile");
+
+				this.properties = new PropertiesFile();
+				try {
+					this.properties.load(new FileInputStream(nomFichier));
+				}
+				catch (IOException var5) {
+					this.properties = null;
+					throw new BatchSystemException("[" + nomFichier + "] impossible d'accéder au fichier de propriétés", var5);
+				}
+			}
+		}
+
+		return this.properties;
+	}
+
+	public String getPropertiesFileName() {
+		return "unireg-metier.properties";
+	}
+
+
+}
